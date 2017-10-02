9e75c8fb36de7c09b01a6c1c0d27c45f02fed567
Thomas Ziegler <thomas.zglr@googlemail.com>
2017-10-02 12:31:30 +0200

    added new map-types

--- src/Types/MapConst.php
+++ src/Types/MapConst.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace golib\Types;
 
 /**
@@ -7,5 +8,9 @@ namespace golib\Types;
  * @author tziegler
  */
 class MapConst {
-    const TIMER = '966Zht5&_8lk2569hdgf_tm';
+
+    const TIMER = 'MT_966Zht5&_8lk2569hdgf_tm';
+    const AUTOINC = 'iu&ghzlkkd78h_po_t5';
+    const NOCHECK = 'KKTN78&bfUIZUR§4&88PPOEWS;TE';
+
 }
