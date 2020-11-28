39b22ff1d3bbf4457fceb4a93fbdfe7d42ad2f92
Thomas Ziegler <t.ziegler@bigpoint.net>
2020-11-28 10:24:13 +0100

    coverage report with docker

--- docker/coverage.sh
+++ docker/coverage.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 cd ../
-docker run -it --rm -v `pwd`:/opt/project php_8.0-rc-xdebug-image /opt/project/vendor/phpunit/phpunit/phpunit -dxdebug.coverage_enable=1 --coverage-clover /opt/phpstorm-coverage/go_lib@test.xml --whitelist /opt/project/src --no-configuration /opt/project/test --teamcity --cache-result-file=/opt/project/.phpunit.result.cache
+#docker run -it --rm -v `pwd`:/opt/project php_8.0-rc-xdebug-image /opt/project/vendor/phpunit/phpunit/phpunit -dxdebug.mode=coverage --coverage-clover /opt/phpstorm-coverage/go_lib@test.xml --whitelist /opt/project/src --no-configuration /opt/project/test --teamcity --cache-result-file=/opt/project/coverage/.phpunit.result.cache
+docker run -it --rm -v `pwd`:/opt/project php_8.0-rc-xdebug-image /opt/project/vendor/phpunit/phpunit/phpunit --coverage-html /opt/project/coverage/  --whitelist /opt/project/src  /opt/project/test
